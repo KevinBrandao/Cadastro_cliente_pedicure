6266580fabaf3a955e013265d81e537161a21c28
KevinKevinBrandao <kevenbrandaoms@outlook.com>
2022-07-13 22:01:00 -0300

    first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produtos</title>
+</head>
+<body>
+    <h1>Produtos</h1>
+    <a href="/products/create">Criar produto</a>
+
+    <ul>
+        @foreach ($products as $product)
+            <li>{{ $product->name }} - R${{ $product->price }} - {{ $product->description}}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
